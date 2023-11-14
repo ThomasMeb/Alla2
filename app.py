b74909402e30c04da1960d309a51bae62b9b1e19
Thomas Mebarki <113993249+ThomasMeb@users.noreply.github.com>
2023-11-14 18:01:53 +0100

    avancées script final, récup data et calcul features

--- app.py
+++ app.py
@@ -8,7 +8,249 @@ import time
 from sklearn.dummy import DummyClassifier
 from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
+from datetime import timedelta
 
-data = pd.read_csv("data/btc_data.csv", index_col=0)
-data.drop(columns=["market_cap", "difficulty"], inplace=True)
+data = pd.read_csv("data/data_raw.csv", index_col=0)
+
+# Votre clé API
+#api_key = input("Please enter your CryptoCompare API key: ")
+api_key = "d1a8b449f14d5e82bd0ea52d5cb86e9171d99f9c0269a9c6eba892868094abee"
+
+
+
+def get_data(existing_data, api_key):
+      # Assurez-vous que l'index est de type datetime
+      existing_data.index = pd.to_datetime(existing_data.index)
+
+      # Récupère la dernière date dans le DataFrame existant
+      last_date = existing_data.index.max()
+
+      # Convertit la dernière date en timestamp UNIX
+      last_timestamp = int(time.mktime(last_date.timetuple()))
+
+      # Définit le timestamp pour la fin de la journée actuelle
+      end_date = datetime.now().replace(hour=23, minute=59, second=59)
+      end_timestamp = int(time.mktime(end_date.timetuple()))
+
+      # Limite de points de données par requête
+      limit = 2000
+
+      # Calcule le nombre total de jours et de requêtes nécessaires
+      total_days = (end_timestamp - last_timestamp) // (24 * 60 * 60)
+      total_requests = -(-total_days // limit)  # Utilise la division entière arrondie vers le haut
+
+      # Fait plusieurs requêtes pour couvrir la période manquante
+      for i in range(total_requests):
+            # Ajuste le paramètre toTs pour chaque requête
+            toTs = end_timestamp - i * limit * 24 * 60 * 60
+
+            # Construit l'URL de l'API
+            url = f'https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit={limit}&toTs={toTs}&api_key={api_key}'
+
+            # Fait la requête API
+            response = requests.get(url)
+            data = response.json()
+
+            # Crée un DataFrame avec les données obtenues
+            new_data = pd.DataFrame(data['Data']['Data'])
+            new_data['time'] = pd.to_datetime(new_data['time'], unit='s')
+            new_data.set_index('time', inplace=True)
+
+            # Convertissez les index en format standard pour la comparaison
+            existing_data.index = pd.to_datetime(existing_data.index)
+            new_data.index = pd.to_datetime(new_data.index)
+
+            # Filtrez les nouvelles données pour ne garder que celles qui ne sont pas dans existing_data
+            new_data_filtered = new_data[~new_data.index.isin(existing_data.index)]
+
+            # Fusionnez les données filtrées avec les données existantes
+            existing_data = pd.concat([existing_data, new_data_filtered])
+
+      # Triez le DataFrame combiné par index (date)
+      existing_data.sort_index(inplace=True)
+
+      return existing_data
+
+# Utilisation de la fonction
+data = get_data(data, api_key)
+#data.drop(['conversionSymbol', 'conversionType'], axis=1, inplace=True)
 print(data)
+
+
+
+def calculate_progression(data):
+      pd.options.mode.chained_assignment = None  # default='warn'
+      data["progression daily"] = 0.0
+      data["progression tomorrow"] = 0.0
+
+      # Calcul de la progression du lendemain
+      data["progression tomorrow"] = data["close"].values / data["open"].values - 1
+
+      # Calcul de la progression quotidienne
+      data.loc[:, "progression daily"] = data['open'].pct_change()
+
+      # Créer une variable cible binaire : 1 si la progression demain est positive, 0 sinon
+      data['target'] = np.where(data['progression tomorrow'] > 0, 1, 0)
+
+calculate_progression(data)
+
+
+def calculate_ema(btc_data):
+      #Calcul EMA 26
+
+      # Choix du nombre de périodes pour l'EMA. Habituellement, 12, 26 ou 50 sont utilisés.
+      n = 26
+      alpha = 2 / (n + 1)
+
+      # Calcul de l'EMA
+      btc_data['ema_26'] = btc_data['open'].ewm(span=n, adjust=False).mean()
+
+      #Calcul EMA 12
+
+      # Choix du nombre de périodes pour l'EMA. Habituellement, 12, 26 ou 50 sont utilisés.
+      n = 12
+      alpha = 2 / (n + 1)
+
+      # Calcul de l'EMA
+      btc_data['ema_12'] = btc_data['open'].ewm(span=n, adjust=False).mean()
+
+def calculate_macd(btc_data):
+      # Calcul du MACD
+      btc_data['macd'] = btc_data['ema_12'] - btc_data['ema_26']
+
+
+def calculate_rsi(btc_data):
+      # Calcul du RSI 
+
+      # Calculer la différence de prix par rapport à la journée précédente
+      btc_data['delta'] = btc_data['open'].diff()
+
+      # Identifier les gains et les pertes
+      btc_data['gain'] = btc_data['delta'].where(btc_data['delta'] > 0, 0)
+      btc_data['loss'] = -btc_data['delta'].where(btc_data['delta'] < 0, 0)
+
+      # Calculer la moyenne des gains et des pertes sur 14 jours
+      rolling_window = 14
+      btc_data['avg_gain'] = btc_data['gain'].rolling(window=rolling_window).mean()
+      btc_data['avg_loss'] = btc_data['loss'].rolling(window=rolling_window).mean()
+
+      # Calculer le RS (Relative Strength)
+      btc_data['rs'] = btc_data['avg_gain'] / btc_data['avg_loss']
+
+      # Calculer le RSI
+      btc_data['rsi'] = 100 - (100 / (1 + btc_data['rs']))
+
+      # Supprimer les colonnes intermédiaires
+      btc_data.drop(columns=['delta', 'gain', 'loss', 'avg_gain', 'avg_loss', 'rs'], inplace=True)
+
+def calculate_rel_volume(btc_data):
+      # Calcul du volume relatif
+
+      # Période pour calculer la moyenne du volume
+      rolling_window = 14
+
+      # Calculer la moyenne du volume sur la période donnée
+      btc_data['avg_volume'] = btc_data['volumeto'].rolling(window=rolling_window).mean()
+
+      # Calculer le volume relatif
+      btc_data['relative_volume'] = btc_data['volumeto'] / btc_data['avg_volume']
+
+      # Supprimer la colonne de volume moyen intermédiaire si désiré
+      btc_data.drop(columns=['avg_volume'], inplace=True)
+
+
+def calculate_obv(btc_data):
+      # Calcul de l'OBV (On-Balance Volume)
+
+      # Calculer la direction du mouvement des prix
+      btc_data['price_direction'] = btc_data['open'].diff()
+
+      # Calculer l'OBV
+      btc_data['obv'] = btc_data['volumeto'].where(btc_data['price_direction'] > 0, -btc_data['volumeto']).cumsum()
+
+      # Supprimer la colonne intermédiaire de direction des prix
+      btc_data.drop(columns=['price_direction'], inplace=True)
+
+def calculate_atr(btc_data):
+      # Calcul de l'ATR (Average True Range)
+
+      # Calculer la différence de prix de clôture par rapport à la journée précédente
+      btc_data['prev_close'] = btc_data['open'].shift(1)
+
+      # Calculer les trois composantes du True Range
+      btc_data['high_minus_low'] = btc_data['high'] - btc_data['low']
+      btc_data['high_minus_prev_close'] = abs(btc_data['high'] - btc_data['prev_close'])
+      btc_data['low_minus_prev_close'] = abs(btc_data['low'] - btc_data['prev_close'])
+
+      # Déterminer le True Range comme étant le maximum des trois valeurs précédentes
+      btc_data['tr'] = btc_data[['high_minus_low', 'high_minus_prev_close', 'low_minus_prev_close']].max(axis=1)
+
+      # Calculer l'ATR comme étant la moyenne mobile du TR sur une période de 14 jours
+      rolling_window = 14
+      btc_data['atr'] = btc_data['tr'].rolling(window=rolling_window).mean()
+
+      # Supprimer les colonnes intermédiaires
+      columns_to_drop = ['prev_close', 'high_minus_low', 'high_minus_prev_close', 'low_minus_prev_close', 'tr']
+      btc_data.drop(columns=columns_to_drop, inplace=True)
+
+def calculate_bollinger(btc_data):
+      # Calcul des bandes de Bollinger
+
+      rolling_window = 20
+
+      # Moyenne mobile simple
+      btc_data['sma'] = btc_data['open'].rolling(window=rolling_window).mean()
+
+      # Écart-type des prix sur la période
+      btc_data['price_std'] = btc_data['open'].rolling(window=rolling_window).std()
+
+      # Calcul des bandes de Bollinger
+      btc_data['bollinger_upper'] = btc_data['sma'] + (btc_data['price_std'] * 2)
+      btc_data['bollinger_lower'] = btc_data['sma'] - (btc_data['price_std'] * 2)
+
+      # Suppression des colonnes intermédiaires
+      btc_data.drop(columns=['sma', 'price_std'], inplace=True)
+
+def calculate_stoch_osc(btc_data):
+      # Oscillateur Stochastique
+
+      rolling_window = 14
+
+      # Trouver le prix le plus bas et le plus élevé sur la période
+      btc_data['rolling_low'] = btc_data['low'].rolling(window=rolling_window).min()
+      btc_data['rolling_high'] = btc_data['high'].rolling(window=rolling_window).max()
+
+      # Calcul du Stochastic Oscillator
+      btc_data['k'] = 100 * ((btc_data['open'] - btc_data['rolling_low']) / (btc_data['rolling_high'] - btc_data['rolling_low']))
+
+      # Suppression des colonnes intermédiaires
+      btc_data.drop(columns=['rolling_low', 'rolling_high'], inplace=True)
+
+def calculate_momentum(btc_data):
+      # Momentum
+
+      n_days = 10
+
+      # Calcul du Momentum
+      btc_data['momentum'] = btc_data['open'] - btc_data['open'].shift(n_days)
+
+
+def calculate_features(data):
+      calculate_ema(data)
+      calculate_macd(data)
+      calculate_rsi(data)
+      calculate_rel_volume(data)
+      calculate_obv(data)
+      calculate_atr(data)
+      calculate_bollinger(data)
+      calculate_stoch_osc(data)
+      calculate_momentum(data)
+
+calculate_features(data)
+
+
+print(data)
+print("valeurs nulles : " , data.isna().sum())
+duplicates = data.index.duplicated(keep=False)
+print("doublons : ", data[duplicates])
+print(data.loc[data['volumefrom']==0])
